2b4871b14ff71f40f4651a9699fb3f2b678550e5
Andrei Selvanovich <andreis@aaisonline.com>
2024-07-15 15:32:36 -0500

    Hide access credentials DB

--- cypress.config.js
+++ cypress.config.js
@@ -176,9 +176,9 @@ module.exports = defineConfig({
             let connection;
             try {
               connection = await oracledb.getConnection({
-                user: "PDP_DATA_LOCATION",
-                password: "pdp_data_location",
-                connectString: "semarchy-development-migrate.ceaq9jb1xfee.us-east-1.rds.amazonaws.com:1521/ORCL"
+                user: config.env.DB_USER,
+                password: config.env.DB_PASSWORD,
+                connectString: config.env.DB_CONNECT_STRING
               });
 
               const result = await connection.execute(query);
@@ -238,7 +238,10 @@ module.exports = defineConfig({
       accessToken: "",
       idToken: "",
       refreshToken: "",
-      CYPRESS_RECORD_KEY: "d76932dd-1ca1-4d55-99cf-f11331f2187a"
+      CYPRESS_RECORD_KEY: "d76932dd-1ca1-4d55-99cf-f11331f2187a",
+      DB_USER: "PDP_DATA_LOCATION",
+      DB_PASSWORD: "pdp_data_location",
+      DB_CONNECT_STRING: "semarchy-development-migrate.ceaq9jb1xfee.us-east-1.rds.amazonaws.com:1521/ORCL"
     },
     specPattern: "cypress/e2e/**/*.*",
     projectId: "fsxi11",
